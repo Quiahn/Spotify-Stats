2028abe36c1b43521cb155d0907b0ae5da07a472
Sardorbek Omonkulov <somonkulov2018@gmail.com>
2021-07-22 14:47:43 -0400

    Added background blur to Carousel

--- client/src/components/pages/Dashboard/components/CarouselTop.js
+++ client/src/components/pages/Dashboard/components/CarouselTop.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Carousel } from 'react-bootstrap'
-import DonutChart from '../../../misc/visuals/DonutChart';
 
 export default function CarouselTop({ topArtist }) {
 
@@ -11,15 +10,18 @@ export default function CarouselTop({ topArtist }) {
     const carouselItems = topArtist.map(artist => {
         return (
             <Carousel.Item key={keyId++}>
-                <div className="top-artist-div img-black-gradient-overlay" style={{ backgroundImage: "url(" + artist.images[0].url + ")", backgroundSize: 'cover' }} >
-
+                <div className="position-relative">
+                    <div className="top-artist-div img-black-gradient-overlay" style={{ backgroundImage: "url(" + artist.images[0].url + ")" }} >
+                    </div>
+                    <img className="Absolute-Center mobileDisable3" src={artist.images[0].url} alt="" />
                 </div>
+
 
                 <Carousel.Caption >
                     <h4>#{keyId}</h4>
                     <h2>{artist.name}</h2>
                     <p> Total Followes: {artist.followers.total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-                    <button className="btn btn-outline-danger btn-sm">About Artist</button>
+                    <button className="btn btn-outline-success btn-sm">About Artist</button>
                 </Carousel.Caption>
             </Carousel.Item >
         )
